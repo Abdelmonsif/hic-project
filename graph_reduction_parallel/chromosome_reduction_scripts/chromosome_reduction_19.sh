e419ef0de47c7447af3e19802639268c223662ee
Wentao Shi <wentao771@gmail.com>
2020-06-04 16:51:15 -0500

    scripts for each chromosome

---
+++ graph_reduction_parallel/chromosome_reduction_scripts/chromosome_reduction_19.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -q workq
+#PBS -l nodes=1:ppn=20
+#PBS -l walltime=72:00:00
+#PBS -N chr_redu_19
+#PBS -A hpc_michal01
+#PBS -j oe
+
+module purge
+source activate graph
+cd /work/derick/hi-c/hic-project/graph_reduction_parallel/
+python chromosome_reduction.py -edge_dir ../../processed_main_graph/final_edge.h5 -node_dir ../../processed_main_graph/final_node.csv -chr 19 > ./log/chromosome_reduction_19.log 2>&1
+
+
+
+
+
+
+
+
+
+
